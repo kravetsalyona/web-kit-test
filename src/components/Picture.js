d74036b346f68ccc4f8aca5a48cb890ff411cff9
Alyona Kravets <kravetsalyonadenisovna@gmail.com>
2023-07-05 15:01:17 +0300

    add class component

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,36 +1,42 @@
-import { useLayoutEffect } from "react"
+import { Component} from "react"
 import logo from '../logo.svg';
 
 
-export default function Picture () {
+export default class Picture extends Component {
     
-    useLayoutEffect(() => {
+    componentDidMount(){
         const img = document.getElementById('img');
         if (window.webkit) {
             window.webkit.messageHandlers.jsHandler.postMessage(`Image size: ${img.offsetWidth}x${img.offsetHeight}`)
         } else {
             console.log("webkit or something undefined")
         }
-        // alert(`Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`);
-        const loader = () => {
+        alert(`Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`);
+        
+        window.addEventListener('load', this.loader)
+        
+    }
+    componentWillUnmount() {
+        window.removeEventListener('load', this.loader);
+      }
+    loader () {
+        const img = document.getElementById('img');
 
-            console.log('Страница загружена');
-            // const size = `Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`;
-            if (window.webkit) {
-                window.webkit.messageHandlers.jsHandler.postMessage(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`)
-            } else {
-                console.log("webkit or something undefined")
-            }
-            // alert(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`);
-          }
-        window.addEventListener('load', loader)
-        return () => window.removeEventListener('load', loader)
-    })
+        // console.log('Страница загружена');
+        // const size = `Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`;
+        if (window.webkit) {
+            window.webkit.messageHandlers.jsHandler.postMessage(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`)
+        } else {
+            console.log("webkit or something undefined")
+        }
+        alert(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`);
+      }
 
-
+    render() {
     return (<>
                 {/* <p>{size}</p> */}
                 <img id="img" src={logo} className="App-logo" alt="logo" />
 
     </>)
+    }
 }
