852ba93c4f9e95bc45e98d55d3b95fb7ff29457c
Alyona Kravets <kravetsalyonadenisovna@gmail.com>
2023-07-05 14:25:21 +0300

    add mock data and size picture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import './App.css';
 import Title from './components/Title';
 import Timer from './components/Timer';
-import People from './components/People';
-import logo from './logo.svg';
+import RandomPlanet from './components/RandomPlanet';
+import Picture from './components/Picture';
+// import logo from './logo.svg';
 import { Component } from "react";
 
 class App extends Component {
+	state = {
+		showRandomPlanet: true,
+		selectedPerson: null,
+	};
+	toggleRandomPlanet = () => {
+		this.setState((state) => {
+		  return {
+			showRandomPlanet: !state.showRandomPlanet,
+		  };
+		});
+	};
   componentDidMount() {
+		// const img = document.getElementById('img');
+		// alert(`Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`);
 		if (window.webkit) {
 			window.webkit.messageHandlers.jsHandler.postMessage("Did finished")
 		} else {
 			console.log("webkit or something undefined")
 		}
+
 	}
 	render() {
+		const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 		return (
 			<div>
 				<Title />
-        <img id="img" src={logo} className="App-logo" alt="logo" />
-        <Timer />
-		<People />
+		
+        		<Timer />
+        		{planet}
+
+       			<button
+          			className="toggle-planet btn btn-warning btn-lg"
+          			onClick={this.toggleRandomPlanet}
+        		>
+        		Toggle Random Planet
+        		</button>
+				<hr/>
+				<Picture />
 			</div>
 		)
 	}
