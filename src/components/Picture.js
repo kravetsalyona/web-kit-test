3afdbf91c6d3f5348042158164fc4f919a715ae8
Alyona Kravets <kravetsalyonadenisovna@gmail.com>
2023-07-05 15:40:39 +0300

    add  страница загружена

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -5,21 +5,36 @@ import logo from '../logo.svg';
 export default class Picture extends Component {
     
     componentDidMount(){
-        this.loader()
-       
+        const img = document.getElementById('img');
+
+        this.loaderBefore()
+        alert(`Image size: ${img.offsetWidth}x${img.offsetHeight}`)
         window.addEventListener('load', this.loader)
         
     }
     componentWillUnmount() {
         window.removeEventListener('load', this.loader);
       }
+      loaderBefore () {
+        const img = document.getElementById('img');
+
+        // console.log('Страница загружена');
+        // const size = `Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`;
+        alert(`Image size: ${img.offsetWidth}x${img.offsetHeight}`)
+        if (window.webkit) {
+            window.webkit.messageHandlers.jsHandler.postMessage(`Image size: ${img.offsetWidth}x${img.offsetHeight}`)
+        } else {
+            console.log("webkit or something undefined")
+        }
+      }
     loader () {
         const img = document.getElementById('img');
 
         // console.log('Страница загружена');
         // const size = `Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`;
+        alert(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`)
         if (window.webkit) {
-            window.webkit.messageHandlers.jsHandler.postMessage(`Image size: ${img.offsetWidth}x${img.offsetHeight}`)
+            window.webkit.messageHandlers.jsHandler.postMessage(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`)
         } else {
             console.log("webkit or something undefined")
         }
