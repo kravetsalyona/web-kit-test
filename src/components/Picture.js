031b223f45120688cda62620ade1be3e2aefb781
Alyona Kravets <kravetsalyonadenisovna@gmail.com>
2023-07-05 14:42:49 +0300

    delete alert add postMessage

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -6,13 +6,22 @@ export default function Picture () {
     
     useLayoutEffect(() => {
         const img = document.getElementById('img');
-        alert(`Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`);
+        if (window.webkit) {
+            window.webkit.messageHandlers.jsHandler.postMessage(`Image size: ${img.offsetWidth}x${img.offsetHeight}`)
+        } else {
+            console.log("webkit or something undefined")
+        }
+        // alert(`Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`);
         const loader = () => {
 
             console.log('Страница загружена');
             // const size = `Размер изображения: ${img.offsetWidth}x${img.offsetHeight}`;
-
-            alert(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`);
+            if (window.webkit) {
+                window.webkit.messageHandlers.jsHandler.postMessage(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`)
+            } else {
+                console.log("webkit or something undefined")
+            }
+            // alert(`Страница загружена. Image size: ${img.offsetWidth}x${img.offsetHeight}`);
           }
         window.addEventListener('load', loader)
         return () => window.removeEventListener('load', loader)
