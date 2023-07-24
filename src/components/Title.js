65789448606780d77ba5599e21ebfd3c4c6ebae2
Alyona Kravets <kravecad@vtb.ru>
2023-07-24 11:13:15 +0300

    add button getItem key for localStarage

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,9 +2,22 @@ export default function Title(){
   const title = 'Баллы Луны';
   const currentBalance = 'Текущий баланс';
   const countOfPoints = 100;
+  const buttonName = 'Получить токен'
+  const handleClick = () => {
+    let dataToken = localStorage.getItem('WEBBOT/RWT');
+    if (dataToken && window.webkit) {
+      window.webkit.messageHandlers.jsHandler.postMessage(`Token: ${dataToken}`);
+      alert(`Token: ${dataToken}`)
+    }
+    else {
+      alert(`Token: отсутствует`)
+    }
+
+  }
 return (<>
     <h1>{title}</h1>
     <h2>{currentBalance}</h2>
     <p>{countOfPoints}</p>
+    <button onClick={handleClick}>{buttonName}</button>
 </>)
 }
