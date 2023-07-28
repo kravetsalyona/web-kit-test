25f03904933250b483c1b13206c3d80fb8019e3e
Alyona Kravets <kravecad@vtb.ru>
2023-07-28 14:45:59 +0300

    fix

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,22 +1,26 @@
+import { useEffect } from "react";
+
 export default function Title(){
   const title = 'Баллы Луны';
   const currentBalance = 'Текущий баланс';
   const countOfPoints = 100;
-  const aName = 'https://dzen.ru/'
-  const handleClick = () => {
-    // let dataToken = localStorage.getItem('WEBBOT/RWT');
+  useEffect( () =>{
     if (window.webkit) {
       window.webkit.messageHandlers.jsHandler.postMessage(`URL: ${'https://dzen.ru/'}`);
+      window.webkit.messageHandlers.jsHandler.postMessage(`URL: ${'https://dzen.ru/pogoda/saint-petersburg?lat=59.938951&lon=30.315635'}`);
     }
     else {
       alert(`webkit: отсутствует`)
     }
+  },[])
+    
 
-  }
+  
 return (<>
     <h1>{title}</h1>
     <h2>{currentBalance}</h2>
     <p>{countOfPoints}</p>
     <a href='https://dzen.ru/'>Ссылка на Дзен</a>
+    <a href='https://dzen.ru/pogoda/saint-petersburg?lat=59.938951&lon=30.315635'>Ссылка на Дзен-Погода</a>
 </>)
 }
