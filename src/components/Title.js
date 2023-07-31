80639686446efef913875a99b9e516ae6d23d21e
Alyona Kravets <kravecad@vtb.ru>
2023-07-31 13:20:20 +0300

    add classname

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,4 +1,8 @@
+
 import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+import "../styles/title.css";
+import cn from "classnames";
 
 export default function Title(){
   const title = 'Баллы Луны';
@@ -6,35 +10,33 @@ export default function Title(){
   const countOfPoints = 100;
   const dzenURL = 'https://dzen.ru/';
   const dzenWeatherURL = 'https://dzen.ru/pogoda/saint-petersburg?lat=59.938951&lon=30.315635';
-  const [status, setStatus] = useState('true');
+  const [status, setStatus] = useState(true);
   useEffect( () =>{
     if (window.webkit) {
       window.webkit.messageHandlers.jsHandler.postMessage(`URL: ${dzenURL}`);
       window.webkit.messageHandlers.jsHandler.postMessage(`URL: ${dzenWeatherURL}`);
     }
-    else {
-      alert(`webkit: отсутствует`)
-    }
+    // else {
+    //   alert(`webkit: отсутствует`)
+    // }
     
   },[])
   window.updateFromNative = (nativeData) => {
     if (nativeData === dzenURL) {
       setStatus(false);
-      
-
     }
-    
-
   }
-    
 
+  // setTimeout(() => {
+  //   setStatus(false);
+  // }, 4000)
   
 return (<>
     <h1>{title}</h1>
     <h2>{currentBalance}</h2>
     <p>{countOfPoints}</p>
-    <a href='https://dzen.ru/' id="elemDzenURL" disabled={status}>Ссылка на Дзен</a>
+    <a className={cn({isDisabledLink: status}, {isDisabledLink: status})} href='https://dzen.ru/' id="elemDzenURL" >Ссылка на Дзен</a>
     <br/>
-    <a href='https://dzen.ru/pogoda/saint-petersburg?lat=59.938951&lon=30.315635' disabled>Ссылка на Дзен-Погода</a>
+    <a className={cn({isDisabledLink: status}, {isDisabledLink: status})}  href='https://dzen.ru/pogoda/saint-petersburg?lat=59.938951&lon=30.315635' >Ссылка на Дзен-Погода</a>
 </>)
 }
