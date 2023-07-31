e34ab22a09ef8941665b41323d612c070db1b0b2
Alyona Kravets <kravecad@vtb.ru>
2023-07-31 11:19:02 +0300

    add disabled

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,16 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Title(){
   const title = 'Баллы Луны';
   const currentBalance = 'Текущий баланс';
   const countOfPoints = 100;
+  const dzenURL = 'https://dzen.ru/';
+  const dzenWeatherURL = 'https://dzen.ru/pogoda/saint-petersburg?lat=59.938951&lon=30.315635';
+  const [status, setStatus] = useState('true');
   useEffect( () =>{
     if (window.webkit) {
-      window.webkit.messageHandlers.jsHandler.postMessage(`URL: ${'https://dzen.ru/'}`);
-      window.webkit.messageHandlers.jsHandler.postMessage(`URL: ${'https://dzen.ru/pogoda/saint-petersburg?lat=59.938951&lon=30.315635'}`);
+      window.webkit.messageHandlers.jsHandler.postMessage(`URL: ${dzenURL}`);
+      window.webkit.messageHandlers.jsHandler.postMessage(`URL: ${dzenWeatherURL}`);
     }
     else {
       alert(`webkit: отсутствует`)
+    }
+    window.updateFromNative = (nativeData) => {
+      if (nativeData === dzenURL) {
+        setStatus(false);
+        
+
+      }
+      
+
     }
   },[])
     
@@ -20,7 +32,7 @@ return (<>
     <h1>{title}</h1>
     <h2>{currentBalance}</h2>
     <p>{countOfPoints}</p>
-    <a href='https://dzen.ru/'>Ссылка на Дзен</a>
+    <a href='https://dzen.ru/' id="elemDzenURL" disabled={status}>Ссылка на Дзен</a>
     <br/>
     <a href='https://dzen.ru/pogoda/saint-petersburg?lat=59.938951&lon=30.315635'>Ссылка на Дзен-Погода</a>
 </>)
